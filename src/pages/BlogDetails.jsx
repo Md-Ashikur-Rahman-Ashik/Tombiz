8ceb6a54d0d20173568ec3460973476f89de1f48
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-02-10 08:43:25 +0600

    Added consultation section on blog details page

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,9 +1,11 @@
 import EmbraceSection from "../components/BlogDetails/EmbraceSection";
+import FreeConsultation from "../components/ConsultationSection";
 
 const BlogDetails = () => {
   return (
     <>
       <EmbraceSection />
+      <FreeConsultation />
     </>
   );
 };
